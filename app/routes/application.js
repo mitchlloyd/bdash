114f944fe975ef629474940510eed8a97300983d
Mitch Lloyd <mitch.lloyd@gmail.com>
2016-12-13 16:19:36 -0800

    Start

---
+++ app/routes/application.js
@@ -0,0 +1,15 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model() {
+    return [
+      { name: "Mitch", dob: "1983-03-26" },
+      { name: "Joe",   dob: "1960-12-14" },
+      { name: "Ellie", dob: "2012-02-10" },
+      { name: "Brian", dob: "1960-12-15" },
+      { name: "Sally", dob: "1990-01-08" },
+      { name: "Mark",  dob: "1960-12-14" },
+      { name: "Liz",   dob: "1960-12-15" }
+    ];
+  }
+});
